3889cb6b7d936936e6b273f88a5462e2d56c2938
Mykenzie Rogers <mykenzierogers@Mykenzies-MacBook-Pro.local>
2019-04-15 08:55:07 -0500

    add input fields in Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,11 @@ export default class Profile extends Component {
     render() {
         return(
             <div>
-                <input />
+                <input placeholder="Favorite Show" />
+                <input placeholder="Favorite Movie" />
+                <input placeholder="Favorite Color" />
+                <input placeholder="Favorite Food" />
+                <input placeholder="Favorite Band" />
             </div>
         )
     }
